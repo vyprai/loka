30ec9779740aa1a3e8a4bb0b59a912fc4d48243b
Ahmad Rizqi Meydiarso <rizqi@meydiarso.com>
2026-03-23 13:37:50 +0700

    Suppress hydration warning on body

--- docs-site/app/layout.tsx
+++ docs-site/app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>
+      <body suppressHydrationWarning>
         <RootProvider>{children}</RootProvider>
       </body>
     </html>
